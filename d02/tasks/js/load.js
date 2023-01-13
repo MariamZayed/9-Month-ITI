26b6367b22c5ba56c1c5e617f9c32883aa8e27d3
MariamZayed <mariamzayed91@gmail.com>
2023-01-13 21:44:20 +0200

    Add d01 and d02

--- d02/tasks/js/load.js
+++ d02/tasks/js/load.js
@@ -1,20 +1,20 @@
-$(function(){
-    $("#json").on("click",function(){
-        $.ajax({
-            type: "get",
-            url: "../Data/DataObject.json",
-            success:function(res){
-                debugger;
-                $('#json').text('');
-                for(let line in res){
-                    for(let obj in line)
-                        $('textarea').text($('textarea').text()+obj+' : '+line[obj]+'\n');
-                    $('textarea').text($('textarea').text()+'\n')
-                }
-            }
-        }); 
-    })
-})
+// $(function(){
+//     $("#json").on("click",function(){
+//         $.ajax({
+//             type: "get",
+//             url: "../Data/DataObject.json",
+//             success:function(res){
+//                 debugger;
+//                 $('#json').text('');
+//                 for(let line in res){
+//                     for(let obj in line)
+//                         $('textarea').text($('textarea').text()+obj+' : '+line[obj]+'\n');
+//                     $('textarea').text($('textarea').text()+'\n')
+//                 }
+//             }
+//         }); 
+//     })
+// })
 // $('button#JSON').on("click",function(){
 //     $.ajax({
 //         url: "json.json",
