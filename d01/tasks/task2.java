077309eeee2edcb3840acd6a9d3320b9f5143824
MariamZayed <mariamzayed91@gmail.com>
2023-01-22 14:18:08 +0200

    Add task d01

---
+++ d01/tasks/task2.java
@@ -0,0 +1,8 @@
+class task2{
+	public static void main(String args[]){
+		if(args.length==2)
+			System.out.println("your name is: " +args[0]+" "+args[1]);
+		else
+			System.out.println("you must enter fist and last name");
+	}
+}
